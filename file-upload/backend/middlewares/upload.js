f2935e50edb6dc55f57689fad924e89af347be7c
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-06-05 16:57:39 +0200

    multer file upload

---
+++ file-upload/backend/middlewares/upload.js
@@ -0,0 +1,32 @@
+import multer from 'multer';
+
+// const upload = multer({ dest: 'uploads/' });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const allowedFormats = ['png', 'jpg', 'jpeg', 'svg', 'webp', 'heic', 'avif'];
+
+const fileFilter = (req, file, cb) => {
+  const fileExt = file.mimetype.split('/')[1];
+
+  // if (file.mimetype.startsWith('image/')) {
+  if (allowedFormats.includes(fileExt)) {
+    cb(null, true);
+  } else {
+    cb(new Error('File type not allowed', { cause: 400 }));
+  }
+};
+
+const fileSize = 1_048_576 * 2; // 2mb
+
+const upload = multer({ storage, fileFilter, limits: { fileSize } });
+
+export default upload;
---
+++ file-upload/backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import upload from './middlewares/upload.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.static('uploads'));
+
+app.get('/', (req, res) => {
+  res.send('Running');
+});
+
+app.post('/file-upload', upload.single('image'), (req, res) => {
+  // console.log(req.body);
+  console.log(req.file);
+
+  const location = `${req.protocol}://${req.host}/${req.file.filename}`;
+
+  res.json({ message: 'File upload successful', location });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.cause || 500).json({ error: err.message });
+});
+
+app.listen(port, () => console.log(` File Upload Server listening on port ${port} `));
